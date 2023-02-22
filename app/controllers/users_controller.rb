d4f68b835bb6f7a456574f6d511ba50329b23a22
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-23 02:09:23 +0300

    Return totals

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     @total_categories = current_user.categories.size
     @total_expenses = calculate_total
   end
+
+  private
+
+  def calculate_total
+    sum = 0
+    current_user.categories.each do |cat|
+      sum += ExpenseCategory.all.where(category_id: cat.id).size
+    end
+    sum
+  end
 end
