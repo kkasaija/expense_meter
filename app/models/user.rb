00d9584ba62270414a40b43acb8898d2ffe4548a
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-23 01:22:49 +0300

    Implement model associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
+
+  has_many :categories, foreign_key: 'author_id', dependent: :destroy
+  has_many :expenses, foreign_key: 'author_id', dependent: :destroy
 end
